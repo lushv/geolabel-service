a4e82e8d5b9c4eba19fa8179febd7fa6d084a241
lushv <lushv@aston.ac.uk>
2013-10-05 00:59:42 +0100

    Updated demo.html page. All 3 demo tabs are now using the GEO label API end point (.../api/v1/geolabel...).

--- web/js/custom/geolabel-forms.js
+++ web/js/custom/geolabel-forms.js
@@ -1,13 +1,15 @@
 $(document).ready(function() {
 	// Clear all Query Constraints fields
 	$("#loader_1").hide();
-	
+	$("#loader_2").hide();
+	$("#loader_3").hide();
 });
 
+// ************************************************  TAB 1 FUNCTIONS *************************************************
 $(function() {
   $("#submit_btn_1").click(function() {
 	// First of all clear all the previous results
-	$("#geolabel_1").html('');
+	$("#geolabel_1").empty();
 	
 	var http = location.protocol;
 	var slashes = http.concat("//");
@@ -47,6 +49,90 @@ $(function() {
 		  },
 		  error:function(){
 			$("#geolabel_1").html("An error occured.");
+		  },
+		}); // -- End of Ajax request to get dataset details
+	}) // -- End of on label click function
+}); // -- End of $(function() { ... });
+
+// ************************************************  TAB 2 FUNCTIONS *************************************************
+$(function() {
+  $("#submit_btn_2").click(function() {
+	// First of all clear all the previous results
+	$("#geolabel_2").empty();
+	
+	var http = location.protocol;
+	var slashes = http.concat("//");
+	var host = slashes.concat(window.location.hostname) + "/api/v1/geolabel";
+	
+	var onProgressHandler = function() {
+		$('#loader_2').show();
+	}
+	var onLoadHandler = function() {
+		$('#loader_2').hide();
+	}
+	var onErrorHandler = function() {
+		$('#loader_2').hide();
+	}
+	
+    var formData = new FormData($('#geolabel_form_2')[0]);
+	
+	xhr = new XMLHttpRequest();
+	xhr.open("POST", host, false);
+	// Following line is just to be on the safe side; not needed if your server delivers SVG with correct MIME type
+	xhr.overrideMimeType("image/svg+xml");
+	xhr.upload.addEventListener('progress', onProgressHandler, false);
+	xhr.upload.addEventListener('load', onLoadHandler, false);
+	xhr.upload.addEventListener('error', onErrorHandler, false);
+	xhr.send(formData);
+	document.getElementById("geolabel_2").appendChild(xhr.responseXML.documentElement);
+		
+	}) // -- End of on label click function
+}); // -- End of $(function() { ... });
+
+// ************************************************  TAB 3 FUNCTIONS *************************************************
+$(function() {
+  $("#submit_btn_3").click(function() {
+	// First of all clear all the previous results
+	$("#geolabel_3").empty();
+	
+	var http = location.protocol;
+	var slashes = http.concat("//");
+	var host = slashes.concat(window.location.hostname) + "/api/v1/geolabel?";
+	
+	// validate and process form here
+	var metadata_url = $("#metadata_url_3").val();
+	var target_code = $("#target_code_3").val();
+	var target_codespace = $("#target_codespace_3").val();
+	var size = $("#size_3").val();
+	
+	// encode URLs
+	metadata_url = encodeURIComponent(metadata_url);
+	var feedback_url = "";
+	if(target_code != ''){
+		feedback_url = encodeURIComponent('https://geoviqua.stcorp.nl/api/v1/feedback/items/search?target_code=' + target_code + '&target_codespace=' + target_codespace + '&view=full&format=xml');
+	}
+	
+	dataString = 'metadata=' + metadata_url + '&feedback=' + feedback_url + '&size=' + size;
+	
+	// Make a get request using jQuery ajax (see Example 1 for alternative implementation)
+    $.ajax({
+		  type: "GET",
+		  url: host,
+		  dataType: "xml",
+		  mimeType: "image/svg+xml",
+		  data: dataString,
+		  beforeSend: function() {
+			$('#loader_3').show();
+		  },
+		  complete: function(){
+			$('#loader_3').hide();
+		  },
+		  success: function(data){
+			var xml = $(data).find('svg');
+			$("#geolabel_3").append(xml);
+		  },
+		  error:function(){
+			$("#geolabel_3").html("An error occured.");
 		  },
 		}); // -- End of Ajax request to get dataset details
 	}) // -- End of on label click function
